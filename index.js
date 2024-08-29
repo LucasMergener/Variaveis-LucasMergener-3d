55b12b549ef30408475739fd641cf350f6034bc1
LucasMergener <127799242+LucasMergener@users.noreply.github.com>
2024-08-29 01:29:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let anoAtual = 2024;
+const nomeCompleto = "Lucas Mergener de Moura";
+let idade = 16;
+const cursoFaculdade = "Biotecnologia";
+let cidadeAtual = "São José dos Pinhais";
+
+console.log(`Olá eu sou o ${nomeCompleto}, eu tenho ${idade} anos, moro em ${cidadeAtual}, ano que vem estar cursando ${cursoFaculdade}.`);
+
+anoAtual = 2025;
+
+let idadeFutura = 18;
+
+console.log(`Para o ano que vem, o meu nome continuará ${nomeCompleto}, vou ter ${idadeFutura} anos de idade, planejo continuar morando em ${cidadeAtual}, e planejo também estar na faculdade de ${cursoFaculdade}.`);
