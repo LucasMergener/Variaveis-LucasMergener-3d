6cadf921b10d942006573adf753c87aefa44a23f
LucasMergener <127799242+LucasMergener@users.noreply.github.com>
2024-10-10 01:27:19 +0000

    09/10

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ let idadeFutura = 18;
 console.log(`Para o ano que vem, o meu nome continuará ${nomeCompleto}, vou ter ${idadeFutura} anos de idade, planejo continuar morando em ${cidadeAtual}, e planejo também estar na faculdade de ${cursoFaculdade}.`);
 
 //Agência de Viagens
+console.log("-------------------------");
+console.log("          LOGIN          ");
+console.log("-------------------------");
+
+const loginCerto = "login123";
+const senhaCerta = 12345;
+
+var login = entrada("Digite o seu Login:");
+var senha = entrada("Digite a sua senha:");
+
+while(login != loginCerto) {
+  console.log("Usuário não encontrado");
+  login = entrada("Digite o seu Login novamente:");
+  senha = entrada("Digite a sua senha novamente:");
+};
+
+while(senha != senhaCerta) {
+  console.log("Senha incorreta");
+  senha = entrada("Digite a sua senha novamente:");
+};
+
+console.log("-------------------------");
+console.log("     DADOS PESSOAIS      ");
+console.log("-------------------------");
 
 var nomeComprador = entrada("Qual é o seu nome?");
 const cpfComprador = entrada("Qual é o seu CPF?");
@@ -28,6 +52,10 @@ const listadeDestinos = new Array(
   " Tóquio"
 );
 
+console.log("-------------------------");
+console.log("   OPERAÇÃO CONCLUÍDA    ");
+console.log("-------------------------");
+
 console.log(`Olá Senhor ${nomeComprador}, portador do CPF ${cpfComprador}, com idade de ${idadeComprador}, confirmamos que a sua passagem para ${listadeDestinos} foi confirmada.`);
 
 console.log(listadeDestinos[0]);
@@ -36,7 +64,10 @@ console.log("");
 console.log(`Olá Senhor ${nomeComprador}, adicionamos mais uma viagem a sua lista de interesses.`);
 
 listadeDestinos.push("Paris");
-var idadeComprador=20;
+
+console.log("-------------------------");
+console.log(" RESULTADO DA OPERAÇÃO:  ");
+console.log("-------------------------");
 
 if(idadeComprador<18){
   console.log(`Infelizmente não podemos lhe vender uma passagem, pois o senhor(a) é menor de idade.`)
